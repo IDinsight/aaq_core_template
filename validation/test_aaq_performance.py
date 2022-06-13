5e0ed1be845009f4186c57e47e46b3453e91a7ae
v_kt_r <entropysmashers@gmail.com>
2022-06-13 10:26:06 +0300

    add email

--- validation/test_aaq_performance.py
+++ validation/test_aaq_performance.py
@@ -10,190 +10,65 @@ import boto3
 from .utils import S3_Handler
 from datetime import datetime
 from enum import Enum
-
-from git import Repo
 import sys
 
-_ACTIONS = True
+
+def generate_message(result, threshold_criteria):
+    """Generate messages for validation results
+    Warning is set to threshold criteria
+    Parameters
+    ----------
+    result : List[dict]
+        List of commit validation results
+    threshold_criteria : float, 0-1
+        Accuracy cut-off for warnings
+    """
+    current_branch = os.environ["BRANCH"]
+    repo_name = os.environ["REPO"]
+    commit = os.environ["HASH"]
+    ref = os.environ["REF"]
+
+    if result < threshold_criteria:
+        val_message = """
+        [Alert] Accuracy was {accuracy} for {commit_tag} with {commit_message} on branch {branch} of repo {repo_name} below threshold of {threshold_criteria}
+         """.format(
+            accuracy=result,
+            commit_tag=commit,
+            commit_message=ref,
+            branch=current_branch,
+            repo_name=repo_name,
+        )
+
+    message = """
+
+        ------Model Validation Results-----
+
+        {}
+
+        -----------------------------------
+
+        """.format(
+        " .\n".join(val_message)
+    )
+    return message
 
 
-class Modes(Enum):
-    """Models class"""
+def send_notification(
+    content="",
+    topic="arn:aws:sns:ap-south-1:678681925278:praekelt-vaccnlp-developer-notifications",
+):
+    """
+    Function to send notification
+    """
+    # TODO this is dev only. find out notification norms
+    import boto3
 
-    HISTORY = 0
-    LIST = 1
-    COMMIT = 2
-
-
-# def validate_commits(
-#     df_test_source,
-#     df_faq_source,
-#     true_label_column,
-#     query_column,
-#     mode=0,
-#     n_commits=5,
-#     commits=None,
-#     branch="main",
-#     threshold_criteria=0.85,
-# ):
-#     """Loop through a repo object to obtain commits
-#     For each commit, calculate accuracy metrics and generate report
-
-#     Parameters
-#     ----------
-#     df_test_source : str
-#         Location of test data
-#     df_faq_source : str
-#         Location of faqs
-#     true_label_column : str
-#         True labels column name
-#     query_column : str
-#         Column name for queries to test
-#     mode : int, optional
-#         Validation mode, by default 0
-#     n_commits : int, optional
-#         Number of commits to iterate, by default 5
-#     commits : List[str] or str, optional
-#         Input list of commits, by default None
-#     branch: str, optional
-#         Name of branch, by default main
-#     threshold_criteria : float, optional
-#         Threshold for warnings, by default 0.85
-
-#     Returns
-#     -------
-#     str
-#         Warning message
-
-#     Raises
-#     ------
-#     Exception
-#         If validation mode is unhandled
-#     """
-
-#     current_branch = os.environ["BRANCH"]
-#     repo_name = os.environ["REPO"]
-#     n_commits = 1
-
-#     if _ACTIONS:
-
-#         sys.path.append(f"/home/runner/work/{repo_name}/{repo_name}/core_model/")
-
-#     repo = Repo("")
-
-#     if mode == Modes.LIST.value:
-#         results = []
-#         for commit in commits:
-#             repo.git.checkout(commit)
-#             results.append(
-#                 {
-#                     "commit": commit,
-#                     "datetime": commit.committed_datetime,
-#                     "result": run_validation_scoring(
-#                         df_test_source,
-#                         df_faq_source,
-#                         true_label_column,
-#                         query_column,
-#                     ),
-#                 }
-#             )
-
-#     elif mode == Modes.COMMIT.value:
-#         results = []
-#         repo.git.checkout(commits)
-
-#         results.append(
-#             {
-#                 "commit": commits,
-#                 "datetime": commits.committed_datetime,
-#                 "results": run_validation_scoring(
-#                     df_test_source,
-#                     df_faq_source,
-#                     true_label_column,
-#                     query_column,
-#                 ),
-#             }
-#         )
-
-#     elif mode == Modes.HISTORY.value:
-#         latest_n_commits = list(repo.iter_commits(branch, max_count=n_commits))
-#         results = []
-#         for commit in latest_n_commits:
-#             repo.git.checkout(commit)
-#             results.append(
-#                 {
-#                     "commit": commit,
-#                     "datetime": commit.committed_datetime,
-#                     "result": run_validation_scoring(
-#                         df_test_source,
-#                         df_faq_source,
-#                         true_label_column,
-#                         query_column,
-#                     ),
-#                 }
-#             )
-#     else:
-#         raise Exception("Unhandled Validation Mode")
-
-#     message = generate_message(results, threshold_criteria)
-
-#     return message, results
-
-
-# def generate_message(results, threshold_criteria):
-#     """Generate messages for validation results
-#     Warning is set to threshold criteria
-#     Parameters
-#     ----------
-#     results : List[dict]
-#         List of commit validation results
-#     threshold_criteria : float, 0-1
-#         Accuracy cut-off for warnings
-#     """
-
-#     validation_messages = []
-#     for validation_content in results:
-#         if validation_content["result"]["top_3_match_accuracy"] < threshold_criteria:
-#             message = """
-#             [Alert] Accuracy was {accuracy} for {commit_tag} with {commit_message} on {commit_time} below threshold
-#             """.format(
-#                 accuracy=validation_content["result"]["top_3_match_accuracy"],
-#                 commit_tag=validation_content["commit"].hexsha,
-#                 commit_message=validation_content["commit"].summary,
-#                 commit_time=validation_content["datetime"],
-#             )
-#             validation_messages.append(message)
-
-#     message = """
-
-#         ------Model Validation Results-----
-
-#         {}
-
-#         -----------------------------------
-
-#         """.format(
-#         " .\n".join(validation_messages)
-#     )
-#     return message
-
-
-# def send_notification(
-#     content="",
-#     topic="arn:aws:sns:ap-south-1:678681925278:praekelt-vaccnlp-developer-notifications",
-# ):
-#     """
-#     Function to send notification
-#     """
-#     # TODO this is dev only. find out notification norms
-#     import boto3
-
-#     sns = boto3.client("sns", region_name="ap-south-1")
-#     sns.publish(
-#         TopicArn=topic,
-#         Message=content,
-#         Subject="Model Validation Results {}".format(datetime.today().date()),
-#     )
+    sns = boto3.client("sns", region_name="ap-south-1")
+    sns.publish(
+        TopicArn=topic,
+        Message=content,
+        Subject="Model Validation Results {}".format(datetime.today().date()),
+    )
 
 
 class TestPerformance:
@@ -253,7 +128,7 @@ class TestPerformance:
             db_connection.execute(t)
         client.get("/internal/refresh-faqs", headers=headers)
 
-    def test_top_3_performance(self, client, faq_data):
+    def test_top_3_performance(self, client, faq_data, test_params):
         validation_df = self.get_validation_data().sample(100)
 
         for idx, row in validation_df.iterrows():
@@ -267,6 +142,8 @@ class TestPerformance:
             validation_df.loc[idx, "in_top"] = row["FAQ Name"] in top_faq_names
 
         top_k_accuracy = validation_df["in_top"].mean()
-        print(top_k_accuracy)
+        send_notification(
+            content=generate_message(top_k_accuracy, test_params["THRESHOLD_CRITERIA"])
+        )
 
         return top_k_accuracy
