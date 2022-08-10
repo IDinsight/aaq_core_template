116b7b6c850408e0b7d0f04eee37aba4d2fb1729
Sid Ravinutala <sid.ravi1@gmail.com>
2022-08-10 05:05:22 -0400

    Validation improvements (#8)
    * fixed teardown
    
    * clear db before starting test
    
    * performance script improvements
    
    * Making PR changes
    
    * minor bug fix
    
    * formatted message formatting, email works, using pd instead of boto for loading data
    
    * Updated requirement in req_ga
    
    * Update requirements_github_actions.txt
    
    * Update config.yaml
    
    Co-authored-by: ziakhan04 <zia.khan@idinsight.org>
    Co-authored-by: ziakhan04 <86958339+ziakhan04@users.noreply.github.com>
    Co-authored-by: sidravi1 <sid.ravi1@gmail.com>

--- validation/test_aaq_performance.py
+++ validation/test_aaq_performance.py
@@ -5,18 +5,17 @@ import os
 from datetime import datetime
 
 import boto3
+import pandas as pd
 import pytest
 from core_model.app.database_sqlalchemy import db
 from nltk.corpus import stopwords
 from sqlalchemy import text
 
-from .utils import S3_Handler
-
 # This is required to allow multithreading to work
 stopwords.ensure_loaded()
 
 
-def generate_message(result, threshold_criteria):
+def generate_message(result, test_params):
     """Generate messages for validation results
     Warning is set to threshold criteria
     Parameters
@@ -26,20 +25,39 @@ def generate_message(result, threshold_criteria):
     threshold_criteria : float, 0-1
         Accuracy cut-off for warnings
     """
-    current_branch = os.environ.get("BRANCH")
-    repo_name = os.environ.get("REPO")
-    commit = os.environ.get("HASH")
-    ref = os.environ.get("REF")
 
-    if result < threshold_criteria:
-        val_message = """
-        [Alert] Accuracy was {accuracy} for {commit_tag} with {commit_message} on branch {branch} of repo {repo_name} below threshold of {threshold_criteria}
-         """.format(
+    threshold_criteria = test_params["THRESHOLD_CRITERIA"]
+    dataset = test_params["VALIDATION_DATA_PREFIX"]
+
+    if (os.environ.get("GITHUB_ACTIONS") == "true") & (result < threshold_criteria):
+
+        current_branch = os.environ.get("BRANCH")
+        repo_name = os.environ.get("REPO")
+        commit = os.environ.get("HASH")
+
+        val_message = (
+            "[Alert] Accuracy using dataset {dataset} was:\n\n"
+            "{accuracy}\n\n"
+            "For commit tag = {commit_tag}\n"
+            "On branch {branch}\n"
+            "Repo {repo_name}\n\n"
+            "The threshold criteria was {threshold_criteria}"
+        ).format(
             accuracy=result,
+            dataset=dataset,
             commit_tag=commit,
-            commit_message=ref,
             branch=current_branch,
             repo_name=repo_name,
+            threshold_criteria=threshold_criteria,
+        )
+    else:
+        val_message = (
+            "[Alert] Accuracy using dataset {dataset} was:\n\n"
+            "{accuracy}\n\n"
+            "Accuracy was ABOVE threshold of {threshold_criteria}"
+        ).format(
+            accuracy=result,
+            dataset=dataset,
             threshold_criteria=threshold_criteria,
         )
 
@@ -78,8 +96,6 @@ class TestPerformance:
     s3r = boto3.resource("s3")
     bucket = os.getenv("VALIDATION_BUCKET")
 
-    s3_handler = S3_Handler(s3, s3r, bucket)
-
     insert_faq = (
         "INSERT INTO faqmatches ("
         "faq_tags, faq_author, faq_title, faq_content_to_send, "
@@ -87,25 +103,23 @@ class TestPerformance:
         "VALUES (:faq_tags, :author, :title, :content, :added_utc, :threshold)"
     )
 
-    def get_validation_data(self):
+    def get_validation_data(self, test_params):
         """
         Download validation data from s3
         """
 
-        prefix = os.getenv("VALIDATION_DATA_PREFIX")
-
-        validation_data = self.s3_handler.load_dataframe_from_object(prefix)
+        prefix = test_params["VALIDATION_DATA_PREFIX"]
+        validation_data = pd.read_csv("s3://" + os.path.join(self.bucket, prefix))
 
         return validation_data
 
-    def get_validation_faqs(self):
+    def get_validation_faqs(self, test_params):
         """
         Download faq data from s3
         """
 
-        prefix = os.getenv("VALIDATION_FAQ_PREFIX")
-
-        faq_df = self.s3_handler.load_dataframe_from_object(prefix)
+        prefix = test_params["VALIDATION_FAQ_PREFIX"]
+        faq_df = pd.read_csv("s3://" + os.path.join(self.bucket, prefix))
 
         return faq_df
 
@@ -123,12 +137,18 @@ class TestPerformance:
         return row[test_params["TRUE_FAQ_COL"]] in top_faq_names
 
     @pytest.fixture(scope="class")
-    def faq_data(self, client, db_engine):
+    def faq_data(self, client, db_engine, test_params):
 
-        self.faq_df = self.get_validation_faqs()
+        self.faq_df = self.get_validation_faqs(test_params)
 
         headers = {"Authorization": "Bearer %s" % os.getenv("INBOUND_CHECK_TOKEN")}
         with db_engine.connect() as db_connection:
+
+            # First, delete any stragglers in the DB from previous runs
+            t = text("DELETE FROM faqmatches WHERE faq_author='Validation author'")
+            with db_connection.begin():
+                db_connection.execute(t)
+
             inbound_sql = text(self.insert_faq)
             inserts = [
                 {
@@ -141,6 +161,7 @@ class TestPerformance:
                 }
                 for idx, row in self.faq_df.iterrows()
             ]
+
             # We do a bulk insert to be more efficient
             with db_connection.begin():
                 db_connection.execute(inbound_sql, inserts)
@@ -162,16 +183,21 @@ class TestPerformance:
         """
         monkeypatch.setattr(db.session, "add", lambda x: None)
 
-        validation_df = self.get_validation_data()
+        validation_df = self.get_validation_data(test_params)
         responses = [
             self.submit_one_inbound(x, client, faq_data, test_params)
             for _, x in validation_df.iterrows()
         ]
         results = list(responses)
         top_k_accuracy = sum(results) / len(results)
-        content = generate_message(top_k_accuracy, test_params["THRESHOLD_CRITERIA"])
-        if os.environ.get("GITHUB_ACTIONS") is True:
+        content = generate_message(top_k_accuracy, test_params)
+
+        if (os.environ.get("GITHUB_ACTIONS") == "true") & (
+            top_k_accuracy < test_params["THRESHOLD_CRITERIA"]
+        ):
             send_notification(content)
+            print(content)
         else:
             print(content)
+
         return top_k_accuracy
