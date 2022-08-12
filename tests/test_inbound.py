295aa254fb7ae47f05850492ba28e66beba60535
Suzin You <suzinyou.sy@gmail.com>
2022-08-12 19:42:27 +0530

    add docstring to clarify changes

--- tests/test_inbound.py
+++ tests/test_inbound.py
@@ -47,6 +47,10 @@ class TestInboundMessage:
         client.get("/internal/refresh-faqs", headers=headers)
 
     def test_inbound_returns_6_faqs(self, client, faq_data):
+        """Since we insert only up to 6 messages in this test and the top_n_matches 
+        is set to 10 in app config, the response will only return 6 faq names. 
+        
+        TODO: parametrize the test based on top_n_matches"""
         request_data = {
             "text_to_match": "I love going hiking. What should I pack for lunch?",
             "return_scoring": "true",
