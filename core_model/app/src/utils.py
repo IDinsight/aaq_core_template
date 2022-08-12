ea1c4df94e2c35a370fe290ef5f2eb68e2c0416d
ziakhan04 <zia.khan@idinsight.org>
2022-08-12 12:58:20 +0530

    Workaround Gensim bug to load model from S3

--- core_model/app/src/utils.py
+++ core_model/app/src/utils.py
@@ -11,7 +11,7 @@ import numpy as np
 import pandas as pd
 import yaml
 from gensim.models import KeyedVectors
-from gensim.models.fasttext import load_facebook_vectors
+from gensim.models.fasttext import load_facebook_model
 
 
 def load_wv_pretrained_bin(folder, filename, model_type="w2v"):
@@ -31,7 +31,7 @@ def load_wv_pretrained_bin(folder, filename, model_type="w2v"):
                 model = load_facebook_vectors(tf.name)
         else:
             full_path = Path(__file__).parents[3] / "data" / folder / filename
-            model = load_facebook_vectors(full_path)
+            model = load_facebook_model(full_path).wv
     elif model_type == "w2v":
         if os.getenv("GITHUB_ACTIONS") == "true":
             bucket = os.getenv("WORD2VEC_BINARY_BUCKET")
