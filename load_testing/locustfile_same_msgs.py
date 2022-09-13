7c7e2b30d010c22c55d7a725728fa55bd96aa71f
amir_emami <amirali1376@gmail.com>
2022-09-13 22:25:55 +0530

    cleanup locustfiles

--- load_testing/locustfile_same_msgs.py
+++ load_testing/locustfile_same_msgs.py
@@ -1,32 +1,24 @@
 import os
 from dotenv import load_dotenv
 import numpy as np
-from locust import HttpUser, task  # , events, between, constant, constant_throughput
-
-#################### Import data ####
+from locust import HttpUser, task  # , between, constant, constant_throughput
 
 ### Import env variables
 load_dotenv(dotenv_path="../secrets/app_secrets.env", verbose=True)
 INBOUND_CHECK_TOKEN = os.getenv("INBOUND_CHECK_TOKEN")
 
-#################### Run API calls ####
-
-## Add seed for reproducibility
+### Run API calls
+# Add seed for reproducibility
 np.random.seed(0)
 
+
 class APIUser(HttpUser):
-
-    # wait_time = constant(1)
-
     @task
     def ask_a_question(self):
-
         ### Experiment 1 - same message sent repeatedly
-        question = "is it normal to vomit every day for a week"
-        # print(question_msg_id, question)
-
+        question = "Test question."
         # send question to API
-        response = self.client.post(
+        self.client.post(
             "/inbound/check",
             json={"text_to_match": question, "return_scoring": "false"},
             headers={"Authorization": f"Bearer {INBOUND_CHECK_TOKEN}"},
--- load_testing/locustfile_val_msgs.py
+++ load_testing/locustfile_val_msgs.py
@@ -3,35 +3,32 @@ from dotenv import load_dotenv
 import numpy as np
 from locust import HttpUser, task, events  # , between, constant, constant_throughput
 
+### load custom functions for loading questions from file
 import question_loading_functions
-
-
-#################### Import data ####
 
 ### Import env variables
 load_dotenv(dotenv_path="../secrets/app_secrets.env", verbose=True)
 INBOUND_CHECK_TOKEN = os.getenv("INBOUND_CHECK_TOKEN")
 
-#################### Run API calls ####
-
-## Add seed for reproducibility
+### Run API calls
+# Add seed for reproducibility
 np.random.seed(0)
-
+# import df of real-world questions
 questions_df = question_loading_functions.load_questions()
 
+
 class APIUser(HttpUser):
-
-    # wait_time = constant(1)
-
     @task
     def ask_a_question(self):
-
         ### Experiment 2 - load a question from the validation dataset
-        question_msg_id, question, faq_name, urgent = question_loading_functions.select_a_question(questions_df, add_typo=False)
-        # print(question_msg_id, question)
-
+        (
+            question_msg_id,
+            question,
+            faq_name,
+            urgent,
+        ) = question_loading_functions.select_a_question(questions_df, add_typo=False)
         # send question to API
-        response = self.client.post(
+        self.client.post(
             "/inbound/check",
             json={"text_to_match": question, "return_scoring": "false"},
             headers={"Authorization": f"Bearer {INBOUND_CHECK_TOKEN}"},
